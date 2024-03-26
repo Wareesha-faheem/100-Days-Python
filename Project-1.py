e6fd36178cb2bef318b0eb3f783b97afef392140
Wareesha Faheem <150460460+Wareesha-faheem@users.noreply.github.com>
2024-03-27 01:27:40 +0500

    Create Project-1.py

---
+++ Project-1.py
@@ -0,0 +1,7 @@
+# Band Name Generator
+print("Welcome to Band Name Generator")
+
+city = input("What's the name of the city you grew up in:? \n")
+pet = input("What's your pet name? \n")
+
+print("Your band name could be " + city + " " + pet)
